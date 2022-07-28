c00d4b995512861766997f8729d40a82c750aba7
jst-gunji <go.gunji@stream.co.jp>
2022-07-28 17:52:17 +0900

    propsにイベント追加、Controlsによるpropsの変換が選択肢になるように型定義

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,18 +1,28 @@
 import { FC } from "react";
 import "./button.css";
 
+type Color = "primary" | "default" | "danger";
+
+type Size = "base" | "sm" | "lg";
+
 type Props = {
   children: React.ReactNode;
-  color?: string;
-  size?: string;
+  color?: Color;
+  size?: Size;
+  onClick: () => void;
 };
 
 export const Button: FC<Props> = ({
   children,
   color = "default",
   size = "base",
+  onClick,
 }) => {
-  return <button className={`${color} ${size}`}>{children}</button>;
+  return (
+    <button className={`${color} ${size}`} onClick={onClick}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
